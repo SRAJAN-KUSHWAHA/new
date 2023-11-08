4d1a3a17dd845edaa1bfa5357415028fba5d33d5
SRAJAN-KUSHWAHA <143284918+SRAJAN-KUSHWAHA@users.noreply.github.com>
2023-11-08 20:18:22 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://techysrajan.blogspot.com/`
+					`https://techysrajan.blogspot.com/p/face.html`
 				}`,
 			},
 		};
